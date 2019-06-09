78d93a51b8243ed9e668b36fabadff3c83d6163c
aligator <aligator@suncraft-server.de>
2019-06-09 16:53:20 +0200

    init

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>JDC Lab</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" href="image/logo.png">
+	<link href="css/normalize.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+	<p>JDC Lab</p>
+</body>
+</html>

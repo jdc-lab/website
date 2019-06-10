85e9d87569076c5937d125d31be537bc634d3bf9
Dominik Braun <braun@sternentstehung.de>
2019-06-10 09:59:09 +0200

    added routing framework and template engine

--- index.php
+++ index.php
@@ -1,35 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>JDC Lab</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="icon" type="image/png" href="image/logo.png">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="css/style.css" rel="stylesheet">
+<?php
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"
-            defer=""></script>
-    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous" defer=""></script>-->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"
-            defer=""></script>
-</head>
-<body>
-<header class="container-fluid">
-    <img id="header-logo" src="image/logo.png" alt="JDCware logo">
-    <h1>JDC Lab</h1>
-</header>
-<div id="main" class="container-fluid">
-    Our personal playground for interesting things.
-    <a href="https://github.com/jdc-lab" target="_blank">Github</a>
-</div>
-<footer class="container-fluid">
-    <ul>
-        <li><a href="impressum.php">Impressum</a></li>
-    </ul>
-</footer>
-</body>
-</html>
+require_once 'vendor/autoload.php';
+
+$router = new Klein\Klein();
+
+$router->respond('/[:page]', function ($r) {
+    render($r->page);
+});
+
+/**
+ * @param string $page
+ * @param array $data
+ * @throws \Twig\Error\LoaderError
+ * @throws \Twig\Error\RuntimeError
+ * @throws \Twig\Error\SyntaxError
+ */
+function render(string $page, array $data = [])
+{
+    $loader = new Twig\Loader\FilesystemLoader('tpl');
+    $twig = new Twig\Environment($loader);
+
+    $ext = '.twig';
+
+    $twig->render($page . $ext, $data);
+}
